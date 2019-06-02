58b26a076ddd3ff30b617901b372e2e23a664991
tsubaki <spr.tsubaki.dev@outlook.jp>
2019-06-02 22:22:13 +0900

    add project and get app info

---
+++ AuthoringLUISTrial/AuthoringLUISTrial/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Authoring;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthoringLUISTrial
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type App Id");
+            var appId = Guid.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type Authoring Key");
+            var authoringKey = Console.ReadLine();
+
+            // Create Authoring Client
+            using (var client = new LUISAuthoringClient(new ApiKeyServiceClientCredentials(authoringKey)) { Endpoint = "https://westus.api.cognitive.microsoft.com" })
+            {
+                // Get App
+                var app = await client.Apps.GetAsync(appId);
+
+                // Get ActiveVersion
+                var activeVersion = app.ActiveVersion;
+            }
+        }
+    }
+}
